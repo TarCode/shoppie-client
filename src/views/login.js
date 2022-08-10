a9f9ffa39f2f2fac5f1c41fcdceb08c79bc713e7
tarcode <tarcode33@gmail.com>
2022-08-10 22:31:44 +0200

    Added state to login and register forms

--- src/views/login.js
+++ src/views/login.js
@@ -1,9 +1,16 @@
 export function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>
+    <form onSubmit={() => {
+      console.log('THE FORM VALUES', email, password)
+    }}>
       <h2>Login</h2>
-      <input type="email" placeholder='Email' />
-      <input type="password" placeholder='Password' />
-    </div>
+      <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder='Email' />
+      <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder='Password' />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
--- src/views/register.js
+++ src/views/register.js
@@ -1,10 +1,20 @@
+import { useState } from 'react'
+
 export function Register() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
   return (
-    <div>
+    <form onSubmit={() => {
+      console.log('THE FORM VALUES', email, password)
+    }}>
       <h2>Register</h2>
-      <input type="email" placeholder='Email' />
-      <input type="password" placeholder='Password' />
-      <input type="password" placeholder='Confirm password' />
-    </div>
-  )
+      <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder='Email' />
+      <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder='Password' />
+      <input value={passwordConfirm} onChange={e => setPasswordConfirm(e.target.value)} type="password" placeholder='Confirm password' />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
