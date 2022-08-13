18647bd5c3e721db6244838f2fb7048307d81124
TarCode <tarcode33@gmail.com>
2022-08-13 20:28:41 +0200

    Display shopping list items

--- src/views/list/index.tsx
+++ src/views/list/index.tsx
@@ -1,11 +1,55 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 
 export function List() {
   const { id } = useParams()
+
+  const stringifiedUser = localStorage.getItem('user') as string
+  const user = JSON.parse(stringifiedUser)
+  const getList = () =>
+    axios('http://localhost:8080/items/list/' + id, {
+      headers: {
+        'x-access-token': user.token,
+      },
+    })
+  const { data: items, refetch } = useQuery(['list', id], getList, {
+    select: (response) => {
+      return response.data
+    },
+  })
+
+  const setItemToDelete = (item: any) => {}
+
+  console.log('LIST ITEMS', items)
+
   return (
     <div>
-      <h2>I am the list</h2>
-      <p>{id}</p>
+      <h2>List</h2>
+      {items && !!items.length ? (
+        items.map((item: any) => (
+          <div key={item.id} style={{ display: 'flex' }}>
+            <button className="contrast outline item-btn">
+              <b>{item.name}</b>
+            </button>
+
+            <div className="col">
+              <button role="button" className="contrast outline action-btn">
+                Edit
+              </button>
+            </div>
+            <div className="col">
+              <button onClick={() => setItemToDelete(item)} role="button" className="contrast outline action-btn">
+                Delete
+              </button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>
+          <p>No items added to list</p>
+        </div>
+      )}
     </div>
   )
 }
