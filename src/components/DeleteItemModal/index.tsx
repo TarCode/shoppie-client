4b05ba92ba2daf0adc89640c31404703cf07f491
TarCode <tarcode33@gmail.com>
2022-08-13 21:45:30 +0200

    Added DeleteItemModal component

---
+++ src/components/DeleteItemModal/index.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from 'react-query'
+
+export function DeleteItemModal(props: any) {
+  const { handleSubmit } = useForm()
+  const queryClient = useQueryClient()
+  const mutation = useMutation(
+    (id) =>
+      axios({
+        method: 'delete',
+        url: 'http://localhost:8080/items/' + id,
+        headers: {
+          'x-access-token': props.token,
+        },
+      }),
+    {
+      onSettled: async () => {
+        await queryClient.invalidateQueries()
+        props.setDeleteItemOpen(null)
+      },
+    },
+  )
+
+  const onSubmit = (data: any) => {
+    mutation.mutate(props.item.id)
+  }
+  return (
+    <dialog open={props.deleteItemOpen}>
+      <article>
+        <header>
+          <a onClick={() => props.setDeleteItemOpen(null)} href="#close" aria-label="Close" className="close"></a>
+          Are you sure you want to delete this item?
+        </header>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>{props.item && props.item.name}</label>
+          <button aria-busy={mutation.isLoading ? true : false} className="contrast">
+            Delete item
+          </button>
+        </form>
+      </article>
+    </dialog>
+  )
+}
